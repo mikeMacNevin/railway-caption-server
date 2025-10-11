fa0558122e81a5c62f41bb49497cf02856a29ce6
MikeMacNevin <mikemacnevin@gmail.com>
2025-10-10 23:50:38 -0400

    fixed articles.js dbconfig

--- routes/articles.js
+++ routes/articles.js
@@ -9,11 +9,11 @@ require('dotenv').config();
 
 
 const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.PORT
+  host: process.env.MYSQLHOST || process.env.DB_HOST,  
+  port: process.env.MYSQLPORT || process.env.port,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+  database: process.env.MYSQLDATABASE || process.env.DB_DATABASE,
 };
 
 
